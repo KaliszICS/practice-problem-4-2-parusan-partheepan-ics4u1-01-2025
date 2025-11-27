5baf47f6b8cb8f77efbbd5fae61403e4c882e852
Parusan <parusan.partheepan@student.tdsb.on.ca>
2025-11-27 16:40:58 -0500

    make changes new

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,44 +1,79 @@
 /**
- * Represents a Dog with name, breed, and weight
+ * Represents a Dog with name, breed, and weight attributes.
+ * This class provides methods to access and modify dog properties.
+ * 
+ * @author Parusan
+ * @version 1.0
  */
 public class Dog {
     private String name;
     private String breed;
-    private int weight;  // CHANGED FROM double TO int
+    private int weight;
 
     /**
-     * Constructor for Dog class
-     * @param name name of dog
-     * @param breed breed of dog
-     * @param weight weight of dog
+     * Constructs a new Dog with the specified name, breed, and weight.
+     *
+     * @param name the name of the dog, cannot be null
+     * @param breed the breed of the dog, cannot be null
+     * @param weight the weight of the dog in pounds, must be positive
      */
-    public Dog(String name, String breed, int weight) {  // CHANGED TO int
+    public Dog(String name, String breed, int weight) {
         this.name = name;
         this.breed = breed;
         this.weight = weight;
     }
 
+    /**
+     * Returns the name of this dog.
+     *
+     * @return the dog's name as a String
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of this dog.
+     *
+     * @param name the new name for the dog, cannot be null
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the breed of this dog.
+     *
+     * @return the dog's breed as a String
+     */
     public String getBreed() {
         return breed;
     }
 
+    /**
+     * Sets the breed of this dog.
+     *
+     * @param breed the new breed for the dog, cannot be null
+     */
     public void setBreed(String breed) {
         this.breed = breed;
     }
 
-    public int getWeight() {  // CHANGED RETURN TYPE TO int
+    /**
+     * Returns the weight of this dog.
+     *
+     * @return the dog's weight in pounds as an integer
+     */
+    public int getWeight() {
         return weight;
     }
 
-    public void setWeight(int weight) {  // CHANGED PARAMETER TO int
+    /**
+     * Sets the weight of this dog.
+     *
+     * @param weight the new weight for the dog in pounds, must be positive
+     */
+    public void setWeight(int weight) {
         this.weight = weight;
     }
 }
